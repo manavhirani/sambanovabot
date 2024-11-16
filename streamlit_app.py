96f0de960e6aa49945b629f99c821dd150ffd601
Vader <manavhirani@gmail.com>
2024-11-16 14:30:09 -0500

    A: Configured SambNova API

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,20 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that uses SambaNovaCloud to generate responses. "
+    "To use this app, you need to provide Samba Nova API key, which you can get [here](https://cloud.sambanova.ai/apis). "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+samba_nova_api_key = st.text_input("SambaNova API Key", type="password")
+if not samba_nova_api_key:
+    st.info("Please add your SambaNova API key to continue.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(
+            base_url="https://api.sambanova.ai/v1",
+            api_key=samba_nova_api_key
+        )
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -30,8 +29,6 @@ else:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
     if prompt := st.chat_input("What is up?"):
 
         # Store and display the current prompt.
@@ -39,9 +36,8 @@ else:
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="Meta-Llama-3.1-8B-Instruct",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
